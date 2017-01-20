2ba31e0e242fd02c7f3fbc32e48b0bfcbf876bf6
santi_s <santsasa@gmail.com>
2017-01-20 15:50:47 -0600

    Call HEC_DSS tab-separated data from GUI Tab-separated data originated from HEC-DSSVue is now store in GUI's handles

--- Import_CSV.m
+++ Import_CSV.m
@@ -2,8 +2,8 @@
 %%                  Import CSV created with HEC-DSSVue                    %
 %:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::%
 %-------------------------------------------------------------------------%
-% This function is used to extract data from a CSV files that was created %
-% with HEC-DSSVue.
+% This function is used to extract data from a tab-separated file that    %
+% with data from a HEC-DSS file
 % References:                                                             %
 % Goodell, C.R. 2014.                                                     %
 % Breaking the HEC-RAS Code: A User's Guide to Automating HEC-RAS. A User's
@@ -19,10 +19,19 @@
 % Outputs:
 % Copyright 2017 Santiago Santacruz & Tatiana Garcia
 %:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::%
+function [dss_data] = Import_CSV(filename)
+    fid = fopen(filename);
+    check_fid(fid)
+    dss_data = {};
+    dss_data.parts = textscan(fid, '%s %s', 5);
+    dss_data.units = textscan(fid, '%s %s', 1);
+    dss_data.type = textscan(fid, '%s %s', 1);
+    dss_data.data = textscan(fid, '%s %s %s %f');
+    fclose(fid)
+end
 
-fid = fopen('dss_text.dat');
-parts = textscan(fid, '%s %s', 5);
-units = textscan(fid, '%s %s', 1);
-type = textscan(fid, '%s %s', 1);
-data = textscan(fid, '%s %s %s %f');
-fclose(fid)
+function check_fid(fid)
+    if fid == -1
+        error('Your file could not be read')
+    end
+end
