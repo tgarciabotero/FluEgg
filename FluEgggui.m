9e43dc42a62122e5eb696404f0be2e656815a6f5
Tatiana Garcia Botero <tatiana.tgarcia@gmail.com>
2018-01-31 00:06:22 -0600

    Update FluEgggui.m

--- FluEgggui.m
+++ FluEgggui.m
@@ -1,7 +1,7 @@
 %==============================================================================
 % FluEgg -Fluvial Egg Drift Simulator
 %==============================================================================
-% Copyright (c) 2013-2017 Tatiana Garcia
+% Copyright (c) 2013 Tatiana Garcia
 
    % This program is free software: you can redistribute it and/or modify
     % it under the terms of the GNU General Public License version 3 as published by
