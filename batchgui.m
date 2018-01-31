c1811e0d3602953723557fe26b8eb27abd561d18
Tatiana Garcia Botero <tatiana.tgarcia@gmail.com>
2018-01-31 00:13:06 -0600

    Update batchgui.m

--- batchgui.m
+++ batchgui.m
@@ -1,3 +1,21 @@
+%==============================================================================
+% FluEgg -Fluvial Egg Drift Simulator
+%==============================================================================
+% Copyright (c) 2013 Tatiana Garcia
+
+   % This program is free software: you can redistribute it and/or modify
+    % it under the terms of the GNU General Public License version 3 as published by
+    % the Free Software Foundation (currently at http://www.gnu.org/licenses/agpl.html) 
+    % with a permitted obligation to maintain Appropriate Legal Notices.
+
+    % This program is distributed in the hope that it will be useful,
+    % but WITHOUT ANY WARRANTY; without even the implied warranty of
+    % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    % GNU General Public License for more details.
+
+    % You should have received a copy of the GNU General Public License
+    % along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
 function varargout = batchgui(varargin)
 % BATCHGUI MATLAB code for batchgui.fig
 %      BATCHGUI, by itself, creates a new BATCHGUI or raises the existing
